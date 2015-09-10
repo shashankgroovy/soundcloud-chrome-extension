dcbf4a75a66b9ec21cae4721647a60fcf92a6d70
Shashank Srivastav <shashankgrovy@gmail.com>
2015-09-10 20:24:57 +0530

    Checks all tabs for soundcloud

--- background.js
+++ background.js
@@ -2,17 +2,35 @@ var status = "pause";
 
 function toggleIconAndTitle() {
 
+  // check if soundcloud tab is found or not
+  getSoundcloudTab();
+
   if (status == 'pause') {
     chrome.browserAction.setIcon({path:"iconplay.png"});
     chrome.browserAction.setTitle({title: "Play song on SoundCloud"});
     status = 'playing'
-    console.log('Playing -- on Soundcloud');
+    console.log('Not Playing');
   } else {
     chrome.browserAction.setIcon({path:"iconpause.png"});
     chrome.browserAction.setTitle({title: "Pause current song"});
     status = 'pause';
-    console.log('Song Paused -- on Soundcloud');
+    console.log('Playing');
   };
+};
+
+function getSoundcloudTab() {
+  // get list of all soundcloud tabs
+  
+  chrome.tabs.query({url: 'https://soundcloud.com/*', currentWindow: true}, function(tabs) {
+    if (tabs.length > 0) {
+      console.log('Soundcloud is present.');
+      console.log("list:" , tabs);
+      return true;
+    } else {
+      console.log('No soundcloud instance found.');
+      return false;
+    }
+  });
 };
 
 chrome.browserAction.onClicked.addListener(toggleIconAndTitle);
