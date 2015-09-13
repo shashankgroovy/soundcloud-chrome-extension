072ac8ecbbfb4cd9afcf85421b73e1e13aca36c9
Shashank Srivastav <shashankgrovy@gmail.com>
2015-09-13 19:57:42 +0530

    Let the music play. Closes #2

--- content.js
+++ content.js
@@ -1,10 +1,14 @@
 // inject the content-script
+var playButton = document.querySelector('.playControl');
+var prevButton = document.querySelector('.skipControl__previous');
+var nextButton = document.querySelector('.skipControl__next');
+var trackInfo = document.querySelector('.playbackSoundBadge__title');
 
-// update exact DOM position of player. use XPATH? :/
-var player = document.getElementById('app');
-
+// function play();
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log('accessing the content-script');
-  if (request.msg == 'getDOM')
-    sendResponse({msg: '#shrank', data: player});
+  if (request.msg == 'getDOM') {
+    playButton.click();
+    sendResponse({msg: '#shrank', data: playButton.title});
+  }
 });
